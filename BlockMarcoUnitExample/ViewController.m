4ec3fb1eef76ead372d980f90ef6ac87a05bc211
levinXiao <772381545@qq.com>
2018-02-05 15:57:18 +0800

    init commit

--- BlockMarcoUnitExample/ViewController.m
+++ BlockMarcoUnitExample/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "BlockMarco.h"
+
 @interface ViewController ()
 
 @end
@@ -17,6 +19,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //origin type
+    __weak __typeof__(self) weakSelf = self;
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        __strong __typeof(self) strongSelf = weakSelf;
+        
+        [strongSelf doSomething];
+        [strongSelf doOtherThing];
+    });
+    
+    //easy type
+    @weakify(self)
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        @strongify(self)
+        
+        [self doSomething];
+        [self doOtherThing];
+    });
+    
+
+}
+
+- (void)doSomething{
+    NSLog(@"doSomething");
+}
+
+- (void)doOtherThing{
+    NSLog(@"doOtherThing");
 }
 
 
